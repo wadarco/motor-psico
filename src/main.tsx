4c9260ebc99f3631b002ce992977d43794aa7246
Nyarlathotep <anhedonia@skiff.com>
2025-10-22 00:34:30 +0000

    refactor(server): clean up and simplify socket

--- src/main.tsx
+++ src/main.tsx
@@ -5,34 +5,18 @@ Bun.serve({
   routes: {
     '/*': index,
     '/socket': (req, server) => {
-      const success = server.upgrade(req, {
-        data: { connectionTag: `${crypto.randomUUID()}` },
-      })
-      if (!success) {
-        return new Response('WebSocket upgrade failed for /socket', {
-          status: 400,
-        })
-      }
+      const success = server.upgrade(req)
+      return !success
+        ? new Response('WebSocket upgrade error', { status: 400 })
+        : undefined
     },
   },
 
   websocket: {
-    data: {} as { connectionTag: string },
-
-    open(ws) {
-      ws.subscribe(ws.data.connectionTag)
-    },
     async message(ws, message) {
       const { from, text } = JSON.parse(message.toString())
       const result = await pandoc({ from, to: 'html', text })
-
       if ('ok' in result) ws.sendText(result.ok, true)
-      // 'err' in result
-      //   ? ws.close(1011, 'Unsupported data')
-      //   : ws.sendText(result.ok, true)
-    },
-    close(ws) {
-      ws.unsubscribe(ws.data.connectionTag)
     },
   },
 
