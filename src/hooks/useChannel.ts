7b0803feb2b69183d81703f98bff2407507e733d
Nyarlathotep <anhedonia@skiff.com>
2025-12-03 07:27:56 -0500

    fix(channel): side effects & resource cleanup
    resolve uncontrolled side effects and ensure proper resource cleanup.

--- src/hooks/useChannel.ts
+++ src/hooks/useChannel.ts
@@ -1,4 +1,11 @@
-import { type RefObject, useCallback, useEffect, useRef, useState } from 'react'
+import {
+  type RefObject,
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react'
 import {
   type ChannelBridge,
   ChannelFactory,
@@ -8,31 +15,43 @@ import {
 export function useChannel<Send, Receive>(
   type: string,
   listenTo: string,
-  target: RefObject<Worker | Window | null>,
+  targetRef: RefObject<Worker | Window | null>,
 ) {
   const [message, setMessage] = useState<Receive>()
   const channelRef = useRef<ChannelBridge>(null)
-  const MsgBuilder = useRef(MessageBuilder.of(type).withPayload<Send>())
+  const msgBuilder = useMemo(
+    () => MessageBuilder.of(type).withPayload<Send>(),
+    [type],
+  )
 
   const sendMessage = useCallback(
-    (data: Send) =>
-      void channelRef.current?.send(MsgBuilder.current.build(data)),
-    [],
+    (data: Send) => {
+      if (!channelRef.current) return
+      const payload = msgBuilder.build(data)
+      return channelRef.current.send(payload)
+    },
+    [msgBuilder],
   )
 
   useEffect(() => {
-    if (!target.current) return
-    const _target = target.current
+    if (!targetRef.current) return
+    const target = targetRef.current
+    channelRef.current ??= ChannelFactory.createAndTransfer(target)
 
-    target.current.addEventListener(
-      'load',
-      () => {
-        channelRef.current = ChannelFactory.createAndTransfer(_target)
-        channelRef.current.on(listenTo, setMessage)
-      },
-      { once: true },
-    )
-  }, [target.current, listenTo])
+    const channel = channelRef.current
+    const subscribe = () => channel.on(listenTo, setMessage)
+    const isWindow = target instanceof Window
+
+    isWindow && target.document.readyState !== 'complete'
+      ? target.addEventListener('load', subscribe, { once: true })
+      : subscribe()
+
+    return () => {
+      channel.close()
+      channelRef.current = null
+      if (isWindow) target.removeEventListener('load', subscribe)
+    }
+  }, [targetRef.current, listenTo])
 
   return { message, sendMessage }
 }
